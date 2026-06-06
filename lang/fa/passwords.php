a923a9f94a8f637bb5256c433a4dbefe6a3b228b
JustASimpleMahdi <just.a.simple.mahdi@gmail.com>
2026-06-06 18:08:26 +0330

    add lang fa

--- lang/fa/passwords.php
+++ lang/fa/passwords.php
@@ -9,14 +9,14 @@ return [
     |
     | The following language lines are the default lines which match reasons
     | that are given by the password broker for a password update attempt
-    | outcome such as failure due to an invalid password / reset token.
+    | has failed, such as for an invalid token or invalid new password.
     |
     */
 
-    'reset' => 'Your password has been reset.',
-    'sent' => 'We have emailed your password reset link.',
-    'throttled' => 'Please wait before retrying.',
-    'token' => 'This password reset token is invalid.',
-    'user' => "We can't find a user with that email address.",
+    'reset' => 'رمز عبور شما با موفقیت بازیابی شد',
+    'sent' => 'ایمیلی جهت بازیابی رمزعبور برای شما ارسال شد',
+    'throttled' => 'لطفا اندکی صبر کنید و دوباره امتحان کنید',
+    'token' => 'مشخصه بازیابی رمزعبور شما صحیح نمی باشد',
+    'user' => "کاربری با این اطلاعات وجود ندارد",
 
 ];
