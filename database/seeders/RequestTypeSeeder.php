c5a33e7f17b7864f1add9f3462a00b7b599f2139
JustASimpleMahdi <just.a.simple.mahdi@gmail.com>
2026-06-08 19:37:48 +0330

    bugfix

--- database/seeders/RequestTypeSeeder.php
+++ database/seeders/RequestTypeSeeder.php
@@ -14,19 +14,19 @@ class RequestTypeSeeder extends Seeder
     public function run(): void
     {
 
-        RequestType::create([
+        RequestType::createQuietly([
             'title' => DefaultRequestTypeNameEnum::LEAVE_REQUEST->value,
             'name' => DefaultRequestTypeNameEnum::LEAVE_REQUEST->name,
             'description' => ' توضیحات درخواست مرخصی(تاریخ ، مدت ، دلیل)...',
             'readonly' => false
         ]);
-        RequestType::create([
+        RequestType::createQuietly([
             'title' => DefaultRequestTypeNameEnum::BROKEN_REPORT->value,
             'name' => DefaultRequestTypeNameEnum::BROKEN_REPORT->name,
             'description' => 'نوع مشکل ، سیستم مربوطه ، توضیحات کامل...',
             'readonly' => false
         ]);
-        RequestType::create([
+        RequestType::createQuietly([
             'title' => DefaultRequestTypeNameEnum::RESIGNATION_REQUEST->value,
             'name' => DefaultRequestTypeNameEnum::RESIGNATION_REQUEST->name,
             'description' => 'دلیل استعفا ، هماهنگی های لازم ...',
